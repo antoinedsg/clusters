7ef5c5ada5ece368bd3d696f2b8e2f060c79c132
antoinedsg <78203523+antoinedsg@users.noreply.github.com>
2023-02-22 14:53:57 +0800

    Add files via upload

--- greedy.js
+++ greedy.js
@@ -5,43 +5,193 @@ $(document).ready(function () {
 });
 
 //////////////////
-function greedyElement() {
-  var greedyElement;
+function greedyElement(){ 
   var b = document.getElementById("bValue").value;
   var c = document.getElementById("cValue").value;
   var d1 = document.getElementById("d1Value").value;
   var d2 = document.getElementById("d2Value").value;
 
-  // If d1 and d2 are negative, the greedy element is a cluster monomial in the initial cluster
+  // There are six cases, labelled as in LLZ.
+  // Case 1
   if (d1 <= 0 && d2 <= 0) {
-    var exponent1 = -d1;
-    var exponent2 = -d2;
+    case1(d1,d2);
+  }
 
-    // For rendering purposes, we distinguish the cases d1 =0, d2 = 0 and the rest
-    if (d1 == 0 && d2 == 0) {
-      greedyElement = 1;
-    }
-    else if (d1 == 0 && d2 < 0) {
-      greedyElement = `x_2^{${-d2}}`;
-    }
-    else if (d1 < 0 && d2 == 0) {
-      greedyElement = `x_1^{${-d1}}`;
-    }
-    else {
-      greedyElement = `x_1^{${exponent1}} x_2^{${exponent2}}`;
-    }
-    
-    // Add HTML to display the greedy element 
-    document.getElementById("greedyElementOutput").innerHTML = "The greedy element at (\\(d_1,d_2\\)) = (" +d1 +"," + d2 +  ") is \\[" + greedyElement + ".\\]";
-    // Ask MathJax to render the newly created code in LaTeX
-    MathJax.typeset([greedyElementOutput]);
+  // Cases 2, 3, 4, 5 and 6
+  else {
+    case2(d1,d2);
   }
 
   // In all other cases, the greedy element is not a cluster monomial in the initial cluster.
-  else {
-    document.getElementById("greedyElementOutput").innerHTML = "Currently not supported."
-  }
-
-
+  //else {
+  //  document.getElementById("greedyElementOutput").innerHTML = "Currently not supported."
+  //}
 }
 
+// Function to calculate c(p,q) when [d1,d2] is in Case (1) of LLZ
+function case1(d1,d2) {
+  var greedyElement;
+  var exponent1 = -d1;
+  var exponent2 = -d2;
+  // For rendering purposes, we distinguish the cases d1 =0, d2 = 0 and the rest
+  if (d1 == 0 && d2 == 0) {
+    greedyElement = 1;
+  }
+  else if (d1 == 0 && d2 < 0) {
+    greedyElement = `x_2^{${-d2}}`;
+  }
+  else if (d1 < 0 && d2 == 0) {
+    greedyElement = `x_1^{${-d1}}`;
+  }
+  else {
+    greedyElement = `x_1^{${exponent1}} x_2^{${exponent2}}`;
+  } 
+  
+  // Add HTML to display the greedy element 
+  document.getElementById("greedyElementOutput").innerHTML = "The greedy element at (\\(d_1,d_2\\)) = (" +d1 +"," + d2 +  ") is \\[" + greedyElement + ".\\]";
+  // Ask MathJax to render the newly created code in LaTeX
+  MathJax.typeset([greedyElementOutput]);
+}
+
+function case2(d1,d2) {
+  var b = document.getElementById("bValue").value;
+  var c = document.getElementById("cValue").value;
+  var greedyElementArray =["1+"];
+  // max_n is the largest value of n = p+q such that c(p,q) is non-zero
+  var max_n = math.max(d1,d2);
+  // support is an array of arrays S[0], S[1], ... , S[max_n] where
+  // each S[n] contains the values c(p,q) for which p+q = n
+  // By definition, S[0] consists of the value c(0,0)=1, so S[0] = [1].
+  // S[n+1] is built recursively using the values of S[0], S[1], ... , S[n]
+  var support = [[1]];
+  n=1;
+  while (n <= max_n) {
+    // Create a new array S[n]
+    temp = support.concat([[0]]);
+    support = temp;
+    // Dummy variable which will run from 0 to n 
+    i = 0;
+    while (i <= n) {
+      var p = n-i;
+      var q = i;
+      // At i = 0, we have already created the value c(n,0) (as a placemark in the creation of S[n]).
+      if (i == 0) {
+        // We replace the current value c(n,0) = 0 by the correct value of c(n,0)
+        support[n][i] = function_c(support,p,q);
+        if (support[n][i] == 0){
+          // Do nothing
+        }
+        else if (support[n][i] == 1) {
+          if (b*p == 0) {
+            greedyElementArray.push(`x_2^{${c*q}}` + "+");
+          }
+          else if (c*q == 0) {
+             greedyElementArray.push(`x_1^{${b*p}}` + "+");
+          }
+          else {
+            greedyElementArray.push(`x_1^{${b*p}} x_2^{${c*q}}` + "+");
+          }
+          
+        }
+        else {
+          if (b*p == 0) {
+            greedyElementArray.push(support[n][i] + `x_2^{${c*q}}` + "+");
+          }
+          else if (c*q == 0) {
+             greedyElementArray.push(support[n][i] + `x_1^{${b*p}}` + "+");
+          }
+          else {
+            greedyElementArray.push(support[n][i] + `x_1^{${b*p}} x_2^{${c*q}}` + "+");
+          }
+        }
+      }
+      // For all cases 0 < i <= n, we append the value of c(n-k,k) to the array S[n]
+      else {
+        support[n].push(function_c(support,p,q));
+        if (support[n][i] == 0){
+          // Do nothing
+        }
+        else if (support[n][i] == 1) {
+          if (b*p == 0) {
+            greedyElementArray.push(`x_2^{${c*q}}` + "+");
+          }
+          else if (c*q == 0) {
+             greedyElementArray.push(`x_1^{${b*p}}` + "+");
+          }
+          else {
+            greedyElementArray.push(`x_1^{${b*p}} x_2^{${c*q}}` + "+");
+          }
+        }
+        else {
+          if (b*p == 0) {
+            greedyElementArray.push(support[n][i] + `x_2^{${c*q}}` + "+");
+          }
+          else if (c*q == 0) {
+             greedyElementArray.push(support[n][i] + `x_1^{${b*p}}` + "+");
+          }
+          else {
+            greedyElementArray.push(support[n][i] + `x_1^{${b*p}} x_2^{${c*q}}` + "+");
+          }
+        }
+        
+      }
+      i += 1;
+    }
+    n += 1;
+  }
+  //alert(greedyElementArray)
+  var greedyElementString = ''.concat(...greedyElementArray);
+  var greedyElement = greedyElementString.slice(0,greedyElementString.length-1);
+  // Add HTML to display the greedy element 
+  document.getElementById("greedyElementOutput").innerHTML = "The greedy element at (\\(d_1,d_2\\)) = (" +d1 +"," + d2 +  ") is \\[" + `x_1^{${-d1}} x_2^{${-d2}}`+ "(" + greedyElement + "). \\]";
+  // Ask MathJax to render the newly created code in LaTeX
+  MathJax.typeset([greedyElementOutput]);
+}
+
+function function_c(support,p,q) {
+  var b = document.getElementById("bValue").value;
+  var c = document.getElementById("cValue").value;
+  var d1 = document.getElementById("d1Value").value;
+  var d2 = document.getElementById("d2Value").value;
+  const n = p+q;
+  // Calculate first term of the maximum
+  var term1 = 0;
+  if (p == 0) {
+    // Do nothing
+  }
+  else {
+    var k = 1;
+    while (k <= p) {
+      term1 += math.pow(-1,k-1)*support[n-k][q]*gnrlCombinations(d2-c*q+k-1,k);
+      k+= 1;
+    }
+  }
+  // Calculate second term of the maximum
+  var term2 = 0;
+  if (q == 0) { 
+    // Do nothing
+  }
+    else {
+    var k = 1;
+    while (k <= q) {
+      term2 += math.pow(-1,k-1)*support[n-k][q-k]*gnrlCombinations(d1-b*p+k-1,k);
+      k+= 1;
+    }
+  }
+
+  // Define c to be the maximum of the two terms
+  var c = math.max(term1, term2);
+  return c;
+}
+
+function gnrlCombinations(a,b) {
+  var comb = 0;
+  if (a >=b && b >= 0) {
+    comb = math.combinations(a,b);
+  }
+  else {
+    comb = 0;
+  }
+  return comb;
+
+}
